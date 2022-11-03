d42caf50b399f3661f7748fa280c7ca3bec1ca24
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-11-03 20:22:13 -0300

    agregué los middlewares y ruta

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from "express"
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
 
 // Intancia de express
 const app = express();
@@ -9,4 +12,14 @@ app.listen(app.get("port"), ()=>{
     console.log("estoy en el puerto " + app.get("port"))
 })
 
-console.log("Lista tareas")
+// Midlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extends:true}));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'../public')));
+
+// Rutas
+app.get("/apilistatareas", (req, res)=>{
+    res.send('Esto es una prueba de una peticion get')
+})
