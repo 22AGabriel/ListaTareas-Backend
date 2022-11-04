491ad64c7560c01c1bf743f853d7bfafaf1853aa
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-11-04 19:43:04 -0300

    validaciones para agregar una tarea

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import { borrarTarea, crearTarea, listarTareas } from "../controllers/tareas.controllers";
+import { check } from "express-validator";
 
 const router = Router();
 
 router
   .route("/tareas")
   .get(listarTareas)
-  .post(crearTarea);
+  .post(
+    [
+        check("nombreTarea")
+          .notEmpty()
+          .withMessage("Este campo es obligatorio")
+          .isLength({min: 5})
+          .withMessage("El mínimo de caracteres es de 5")
+    ],
+    crearTarea);
   
 router
   .route("/tareas/:id")
